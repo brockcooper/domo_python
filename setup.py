9eeddbd182cc38e9eb2fe060036baaa8a0da1dee
Brock Cooper <brockcooper@gmail.com>
2018-06-06 08:11:52 -0600

    Get working version of domo_python on pip

--- setup.py
+++ setup.py
@@ -5,15 +5,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="domo_python",
-    version="0.0.1",
+    version="0.0.5",
     author="Big Squid, Inc.",
     author_email="bcooper@bigsquid.com",
     description="Domo API functions to import and export data into usable Pandas dataframes and manage your Domo instancea",
     url="https://github.com/brockcooper/domo_python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
 )
